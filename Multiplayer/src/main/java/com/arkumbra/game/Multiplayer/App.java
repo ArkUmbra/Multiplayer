fb5110fa56083c1b8b7cf0b760fd70f63f0448a9
Luke <Luke@Luke-Windows.home>
2012-10-27 17:45:11 +0100

    First cut

---
+++ Multiplayer/src/main/java/com/arkumbra/game/Multiplayer/App.java
@@ -0,0 +1,17 @@
+package com.arkumbra.game.Multiplayer;
+
+import java.net.ServerSocket;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        ServerSocket server = new ServerSocket();
+    }
+}
